01c5d64f30f5e55514067a2ce5a70936921406f5
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-06 18:31:05 -0800

    Add ColorBox component

---
+++ src/ColorBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ColorBox extends Component {
+  render() {
+    return (
+        <div>
+          hi
+        </div>
+    );
+  }
+}
+
+export default ColorBox;
