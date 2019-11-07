c66b879ba9506b0cca7ebd7bac8f1047268d42cc
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-06 19:05:34 -0800

    Move color pick logic to ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import './ColorBox.css';
+import {choose} from "./helpers";
 
 class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: choose(this.props.allColors),
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  pickColor() {
+    let newColor;
+    do {
+      newColor = choose(this.props.allColors);
+    } while (newColor === this.state.color);
+
+    this.setState({ color: newColor });
+  }
+  handleClick(e) {
+    this.pickColor();
+  }
+
   render() {
     return (
-        <div>
-          hi
+        <div className="ColorBox" style={{backgroundColor: this.state.color}}
+            onClick={this.handleClick}>
         </div>
     );
   }
